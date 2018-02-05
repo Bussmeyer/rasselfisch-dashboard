22a384b5d1eb0db38b89633fafc340378737079e
Thomas Bußmeyer <thomas.bussmeyer@publicispixelpark.de>
2018-02-05 21:42:17 +0100

    make function notation more general.

--- blastit/blastit.js
+++ blastit/blastit.js
@@ -1,27 +1,27 @@
 var Elasticsearch = require('elasticsearch');
 
-exports.toElasticsearch = function (deals) {
+exports.toElasticsearch = function (elasticsearchObjects) {
     var client = new Elasticsearch.Client({
         host: process.env.ELASTICSEARCH_HOST,
         log: process.env.ELASTICSEARCH_LOGLEVEL
     });
 
-    deals.forEach(function (deal) {
-        if (!!deal['48fb37075b475d243a63105efc2c430ada90162e']) {
-            groups = deal['48fb37075b475d243a63105efc2c430ada90162e'].toString().split(",");
+    elasticsearchObjects.forEach(function (elasticsearchObject) {
+        if (!!elasticsearchObject['48fb37075b475d243a63105efc2c430ada90162e']) {
+            groups = elasticsearchObject['48fb37075b475d243a63105efc2c430ada90162e'].toString().split(",");
         }
 
         client.index({
             index: 'deals',
             type: 'deal',
-            id: deal.id,
+            id: elasticsearchObject.id,
             body: {
-                id: deal.id,
-                title: deal.title,
-                value: deal.value,
-                add_time: deal.add_time,
-                update_time: deal.update_time,
-                status: deal.status,
+                id: elasticsearchObject.id,
+                title: elasticsearchObject.title,
+                value: elasticsearchObject.value,
+                add_time: elasticsearchObject.add_time,
+                update_time: elasticsearchObject.update_time,
+                status: elasticsearchObject.status,
                 groups: groups
             }
         }, function (error, response) {
