17976e0a13453dde8cb8336a6cba86893e88af73
Thomas Bußmeyer <thomas.bussmeyer@publicispixelpark.de>
2018-02-05 20:29:17 +0100

    add index creation

---
+++ createIndices.js
@@ -0,0 +1,48 @@
+var Elasticsearch = require('elasticsearch');
+
+
+var client = new Elasticsearch.Client({
+    host: process.env.ELASTICSEARCH_HOST,
+    log: process.env.ELASTICSEARCH_LOGLEVEL
+});
+
+client.indices.delete({
+    index: 'deals'
+}, function (error, response) {
+    if (error) return console.log(error);
+    client.indices.create({
+        waitForActiveShards: 2,
+        updateAllTypes: true,
+        index: 'deals',
+        body: {
+            "mappings": {
+                "deal": {
+                    "properties": {
+                        "id": {
+                            "type": "integer"
+                        },
+                        "title": {
+                            "type": "text"
+                        },
+                        "value": {
+                            "type": "scaled_float",
+                            "scaling_factor": 100
+                        },
+                        "add_time": {
+                            "type": "date",
+                            "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+                        },
+                        "update_time": {
+                            "type": "date",
+                            "format": "yyyy-MM-dd HH:mm:ss||yyyy-MM-dd||epoch_millis"
+                        }
+                    }
+                }
+            }
+        }
+    }, function (error, response) {
+        if (error) return console.log(error);
+    });
+});
+
+
